b9793f0bd684483dbf519e4043602f392e2a03da
Valentina De Mite <valesakura97@gmail.com>
2023-09-11 15:21:46 +0200

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+
+*/
