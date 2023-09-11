d73d544885f617e8d31a1d280e3e839028606463
Valentina De Mite <valesakura97@gmail.com>
2023-09-11 16:16:45 +0200

    added message + bonus

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,15 @@ Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
 
 */
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Hello, it's me!",
+      image:
+        "./assets/img/555193-cat-grumpy-cat-the-little-mermaid-disney-humor.jpg",
+    };
+  },
+}).mount("#app");
